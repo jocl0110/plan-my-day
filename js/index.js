b3266067eaeff58319a3225fcf443a2dfea08539
Jose Luis Izquierdo Hernandez <joseluisizquierdoshernandez@gmail.com>
2025-03-08 11:54:50 -0600

    Add the README file with instructions and add all the necessary styles to the project

--- js/index.js
+++ js/index.js
@@ -1,22 +1,36 @@
 // Elements
-const submitBtnEl = document.getElementById("submit_btn");
+
 const inputEl = document.getElementById("city_input");
 const weatherReportEl = document.getElementById("weather_report");
+const messageEl = document.getElementById("message");
 const loadingEl = document.getElementById("loading");
 const footerEl = document.getElementById("footer");
+const formEl = document.getElementById("form");
 
 // Footer
+const year = new Date().getFullYear();
+footerEl.innerHTML = `<p>&copy;Jose Izquierdo ${year}</p>
+<a href="https://www.linkedin.com/in/jose-luis-izquierdo-hernandez-938064245" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+<a href="https://github.com/jocl0110" target="_blank"><i class="fa-brands fa-github"></i></a>
+<a href="https://x.com/jocl0110" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>`;
 
-submitBtnEl.addEventListener("click", async () => {
+formEl.addEventListener("submit", async (event) => {
+  event.preventDefault();
   const city = inputEl.value.trim();
-
+  messageEl.classList.remove("error", "warning");
   if (!city) {
-    weatherReportEl.textContent = "Please enter a city name.";
+    loadingEl.style.display = "none";
+    weatherReportEl.style.display = "none";
+    messageEl.style.display = "block";
+    messageEl.classList.add("warning");
+    messageEl.textContent = `Please enter a city name`;
     return;
   }
 
   loadingEl.style.display = "block";
   weatherReportEl.innerHTML = "";
+  messageEl.textContent = "";
+  weatherReportEl.style.display = "none";
 
   try {
     //   Get coordinates from city name
@@ -26,7 +40,10 @@ submitBtnEl.addEventListener("click", async () => {
     const geoData = await geoResponse.json();
     console.log("Coordinates", geoData);
     if (!geoData.results || geoData.results.length === 0) {
-      weatherReportEl.textContent = "City not found";
+      loadingEl.style.display = "none";
+      messageEl.style.display = "block";
+      messageEl.classList.add("warning");
+      messageEl.textContent = `⚠️City not found.`;
       return;
     }
 
@@ -44,18 +61,18 @@ submitBtnEl.addEventListener("click", async () => {
     const dailyMinTemps = weatherData.daily.temperature_2m_min;
     const dailyDates = weatherData.daily.time;
 
-    let dailyReport = "<h4>Next Days Forecast:</h4>";
+    let dailyReport = "";
     for (let i = 0; i < dailyDates.length; i++) {
-      dailyReport += `
-    📅 ${dailyDates[i]} <br>
-    🔼 Max: ${dailyMaxTemps[i]}°F <br>
-    🔽 Min: ${dailyMinTemps[i]}°F <br><br>
+      dailyReport += `<p>
+    - <strong>${dailyDates[i]}</strong> temperatures from <strong>${dailyMinTemps[i]}</strong>°F 
+   to <strong>${dailyMaxTemps[i]}°F</strong></p><br>
+   
   `;
     }
     let isDay = "";
     weatherData.current_weather.is_day === 1
-      ? (isDay = `<i class="fa-solid fa-sun"></i>`)
-      : (isDay = `<i class="fa-solid fa-moon"></i>`);
+      ? (isDay = `<i class="fa-solid fa-sun day"></i>`)
+      : (isDay = `<i class="fa-solid fa-moon night"></i>`);
     // Getting Air quality
     const airQualityResponse = await fetch(
       `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${latitude}&longitude=${longitude}&hourly=pm10`
@@ -82,22 +99,33 @@ submitBtnEl.addEventListener("click", async () => {
     } else {
       airQualityStatus = "⚫ Extremely Poor";
     }
-    setTimeout(() => {
-      loadingEl.style.display = "none";
-    }, 2000);
+    // setTimeout(() => {
+    //   loadingEl.style.display = "none";
+    // }, 2000);
+    weatherReportEl.style.display = "block";
     weatherReportEl.innerHTML = `
       <h3>Weather Report for ${name}, ${admin1}, ${country}</h3>
-      🌡️ Temperature: ${temp}°F <br>
-      💨 Wind Speed: ${windSpeed} mph <br>
-      ${isDay} <br>
-      <h4>Air Quality</h4>
-      PM10: ${lastPM10}} µg/m³ <br>
-      Status: ${airQualityStatus}
-      ${dailyReport}
+      <div class="air-quality">
+        <div class="air-quality-header">
+          <h4>Air Quality</h4>
+          <span class="day-night-icon ">${isDay}</span>
+        </div>
+        <p>PM10: ${lastPM10} µg/m³</p>
+        <p>Status: ${airQualityStatus}</p>
+      </div>
+      <div class="weather-info">
+        <p class="temperature">🌡️ Temperature: ${temp}°F</p>
+        <p class="windspeed">💨 Wind Speed: ${windSpeed} mph</p>
+      </div>
+      <div class="forecast">
+        <h4>Next Days Forecast:</h4>
+        ${dailyReport}
+      </div>
     `;
   } catch (error) {
-    console.error("Failed to get weather data", error);
-    weatherReportEl.innerHTML = "Something went wrong. Please try again.";
+    messageEl.classList.add("error");
+    messageEl.style.display = "block";
+    messageEl.textContent = "❌ Something went wrong. Please try again later.";
   } finally {
     loadingEl.style.display = "none";
   }
